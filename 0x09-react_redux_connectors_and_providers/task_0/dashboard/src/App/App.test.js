66125eafb2fb186d6d6d04c4a0cd0db50dd78477
Eba10 <Barkotka@gmail.com>
2022-08-23 14:32:43 -0700

    Test MapStateToProps

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/App/App.test.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/App/App.test.js
@@ -1,42 +1,24 @@
-import { shallow } from 'enzyme';
-import React from 'react';
-import App from './App';
+import { shallow, mount } from "enzyme";
+import React from "react";
+import App, { listNotificationsInitialState, mapStateToProps } from "./App";
+import { StyleSheetTestUtils } from "aphrodite";
+import AppContext, { user, logOut } from "./AppContext";
 
-describe('<App />', () => {
-  it('renders without crashing', () => {
-    const wrapper = shallow(<App />);
-  });
+import { fromJS } from "immutable";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import uiReducer, { initialState } from "../reducers/uiReducer";
 
-  it('contain Notifications component', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('Notifications')).toHaveLength(1);
-  });
+const store = createStore(uiReducer, initialState);
 
-  it('contain Header component', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('Header')).toHaveLength(1);
-  });
+describe("<App />", () => {
+  it("mapStateToProps returns the right object from user Login", () => {
+    let state = fromJS({
+      isUserLoggedIn: true,
+    });
 
-  it('contain Login component', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('Login')).toHaveLength(1);
-  });
+    const result = mapStateToProps(state);
 
-  it('contain Footer component', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('Footer')).toHaveLength(1);
-  });
-
-  it('CourseList with isLoggedIn false', () => {
-    const wrapper = shallow(<App />);
-    wrapper.update();
-    expect(wrapper.find('CourseList')).toHaveLength(0);
-  });
-
-  it('isLoggedIn is true', () => {
-    const wrapper = shallow(<App isLoggedIn />);
-    wrapper.update();
-    expect(wrapper.find('Login')).toHaveLength(0);
-    expect(wrapper.find('CourseList')).toHaveLength(1);
+    expect(result).toEqual({ isLoggedIn: true });
   });
 });
